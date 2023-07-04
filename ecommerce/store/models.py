0156be08c5ec5bed8d6dd25648fdb3b6f8eb7dcf
Arguns <paraskyausa23@gmail.com>
2023-07-04 18:26:36 +0545

    update

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, db_index=True)
+    slug = models.SlugField(max_length=255, unique=True)
+
+    class Meta:
+        verbose_name="Categories"
+    
+    def __str__(self):
+        return self.name
+    
+class Product(models.Model):
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE)
